92c490179d7a285fd9f8b450a796d5013975ec14
somesh-v <162274817+somesh-v@users.noreply.github.com>
2024-05-07 10:42:55 +0530

    Add files via upload

---
+++ exp 3/iipc.js
@@ -0,0 +1,10 @@
+function click(){
+var industry=document.getElementById("industry").value
+document.getElementById("industry1").innerHTML=industry
+var consultancy=document.getElementById("consultancy").value
+document.getElementById("CONSULTANCE1").innerHTML=consultancy
+var amount=document.getElementById("amount").value
+document.getElementById("AMOUNT1").innerHTML=amount
+var faculty=document.getElementById("faculty").value
+document.getElementById("FACULTY1").innerHTML=faculty
+}
